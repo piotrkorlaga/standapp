1f9c6bd0be3ea5496a470152adf89ded43e50e93
Grzegorz Wilusz <grzegorzwilusz@MacBook-Air-Grzegorz.local>
2017-12-06 10:16:39 +0100

    Saving data do FireBase

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,12 +2,25 @@ import React, {Component,} from 'react';
 import { View, TextInput } from 'react-native';
 import {Button} from './Button';
 import {ItemList} from './ItemList';
+import firebase from 'firebase';
 
 export class Item extends Component {
     constructor(props) {
         super(props);
         this.state = {input: ''};
         this.state.inputs = [];
+    }
+
+    saveData() {
+        const {input, inputs} = this.state;
+        const { currentUser } = firebase.auth();                    // getting access to current user in our FBDB -> firebase.auth().currentUset
+        firebase.database().ref(`users/${currentUser.uid}/inputs`)  // get access to our FBDB and make a reference to pointed location (it's path to a JSON data store). Then we made string interpolation.
+                                                                    // there we have a TOP collection of users, then a uid, and then a collection of inputs (it's our DB and JSON schema we created)
+            .push({ input, inputs });    // After making a ref we want to do specific operation in this location. Push made data be saved in DB.
+    }
+
+    deleteData() {
+
     }
 
     render() {
@@ -28,6 +41,7 @@ export class Item extends Component {
                     <ItemList key={index} prop={element} pressDelete={() => {
                         const result = this.state.inputs.filter((el) => el !== element);
                         this.setState({inputs: result});
+                        // linking to FireBase
                     }}/>)
                 }
 
@@ -38,6 +52,7 @@ export class Item extends Component {
                             this.setState({inputs: [...this.state.inputs,this.state.input]});
                             this.inputToClear.clear();
                             this.setState({input: ''});
+                            this.saveData();
                         } else {
                             alert('Pass some data.');
                         }
