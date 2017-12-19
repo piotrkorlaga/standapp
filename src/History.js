a6b2d8a799a9d3eab620d0d11b4897b951e49c28
Grzegorz Wilusz <grzegorzwilusz@MacBook-Air-Grzegorz.local>
2017-12-19 11:44:19 +0100

    Prefinal history card design added

--- src/History.js
+++ src/History.js
@@ -1,16 +1,49 @@
 import React, {Component} from 'react';
-import {Container, Header, Content, List, ListItem, Text, Separator, Card, CardItem} from 'native-base';
+import { Body, Button, Title, Subtitle, Right, Left, Icon, Container, Header, Content, List, ListItem, Text, Separator, Card, CardItem} from 'native-base';
 
 export default class History extends Component {
     render() {
         return (
             <Container>
-                <Header/>
+
                 <Content>
+                    <Card>
 
 
+                        <List>
+                            <Header>
+
+                                <Body>
+                                <Title>Daily stand up</Title>
+                                <Subtitle>Date</Subtitle>
+                                </Body>
+
+                            </Header>
+
+                            <ListItem itemDivider>
+                                <Text>Done today:</Text>
+                            </ListItem>
+                            <ListItem first>
+                                <Text></Text>
+                            </ListItem>
+
+                            <ListItem itemDivider>
+                                <Text>Will be done tomorrow:</Text>
+                            </ListItem>
+                            <ListItem first>
+                                <Text></Text>
+                            </ListItem>
+
+                            <ListItem itemDivider>
+                                <Text>Problems met today:</Text>
+                            </ListItem>
+                            <ListItem first>
+                                <Text></Text>
+                            </ListItem>
+                        </List>
 
 
+                    </Card>
                 </Content>
 
             </Container>
