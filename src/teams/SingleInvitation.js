db95faaa7963859d665f19b577dba110d50e7c6e
Greg <wilusz.gregory@gmail.com>
2018-01-10 16:07:52 +0100

    Confirmation of invitation works

--- src/teams/SingleInvitation.js
+++ src/teams/SingleInvitation.js
@@ -7,34 +7,19 @@ import { Button, Text, Card, CardItem, Right, Left } from 'native-base';
 export class SingleInvitation extends Component {
   constructor() {
     super();
-    this.state = {
-      unacceptedInvitation: [],
-      token: '',
-    };
     this.onConfirmInvitationButtonPress = this.onConfirmInvitationButtonPress.bind(this);
   }
 
   onConfirmInvitationButtonPress() {
-    const idToken = this.state.token;
-    axios.patch(`https://standapp-e73d7.firebaseio.com/v3/users/${firebase.auth().currentUser.uid}.json?auth=${idToken}`, { teamkey: 'nomadit' })
-      .then((response) => {
-        axios.patch(`https://standapp-e73d7.firebaseio.com/v3/users/${firebase.auth().currentUser.uid}/invitations/.json?auth=${idToken}`);
-        console.log('Invitations?', response);
-      })
-    // .then(exactInvitation => axios.patch(`https://standapp-e73d7.firebaseio.com/v3/users/${firebase.auth().currentUser.uid}/invitations.json?auth=${idToken}&orderBy="${exactInvitation.data.id}"&equalTo="123321"`, {
-    //   isAccepted: true,
-    // }))
-    // })
-    // .then((invitations) => {
-    //   console.log('invitations: ', invitations);
-    //   const invitationArray = _.map(invitations.data, (invitationData, id) => new Invitation(invitationData.fromUser, id, invitationData.isRead, invitationData.isAccepted));
-    //       const
-    //       return axios.patch(`https://standapp-e73d7.firebaseio.com/v3/users/${firebase.auth().currentUser.uid}/invitations/${invitations}.json?auth=${idToken}`, isAccepted = {
-    //             isAccepted: true,
-    //         });
-    //   })
-    // })
-      .catch(error => console.log('error with added teamkey ', error));
+    firebase.auth().currentUser.getIdToken(true)
+      .then(idToken => axios.patch(`https://standapp-e73d7.firebaseio.com/v3/users/${firebase.auth().currentUser.uid}.json?auth=${idToken}`, { teamkey: 'nomadit' })
+        .then((response) => {
+          axios.patch(`https://standapp-e73d7.firebaseio.com/v3/users/${firebase.auth().currentUser.uid}/invitations/${this.props.invitation.id}.json?auth=${idToken}`, {
+            isAccepted: true,
+          });
+          console.log('Invitations?', response);
+        }))
+      .catch(error => console.log('Error with confirm of invitation', error));
   }
 
   render() {
